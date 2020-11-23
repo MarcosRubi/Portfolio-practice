901e3542611a4714b4580442d2768daf4ffd1dcd
Daniel <danielhernandez9980@gmail.com>
2020-11-23 12:07:01 -0600

    Responsive

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,10 @@ window.addEventListener('scroll', ()=>{
 
     header.classList.toggle('sticky', window.scrollY > 0);
 })
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.toggle');
+    const menu = document.querySelector('.menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+}
